9d71531afde6a7035f9ee12c3dd67b00a957b2c3
Parvsub <Praveenboda421@gmail.com>
2023-02-07 13:24:42 +0530

    Added:Welcome message is displaying

---
+++ Welcomemessage.java
@@ -0,0 +1,7 @@
+package Com.LineComparisonProblem;
+
+public class Welcomemessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation");
+    }
+}
